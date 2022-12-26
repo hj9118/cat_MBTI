a720dfea1d8c94283103377c97d680f111c3dcb7
jiho9118 <mexocoxon@gmail.com>
2022-12-26 23:45:53 +0900

    Question.js 기능 추가

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button, ProgressBar } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { QuestionData } from '../assets/data/QuestionData';
 
 function Question() {
+  const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
+    { id: 'EI', score: 0 },
+    { id: 'SN', score: 0 },
+    { id: 'TF', score: 0 },
+    { id: 'JP', score: 0 },
+  ]);
+
+  const navigate = useNavigate();
+
+  console.log(totalScore);
+
+  const handleClickButton = (no, type) => {
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + no } : s
+    );
+    setTotalScore(newScore);
+    if (QuestionData.length !== questionNo + 1) {
+      setQuestionNo(questionNo + 1);
+    } else {
+      navigate('/result');
+    }
+  };
+
   return (
     <Wrapper>
       <ProgressBar
         striped
         variant='danger'
-        now={80}
+        now={(questionNo / QuestionData.length) * 100}
         style={{ marginTop: '20px' }}
       />
-      <Title>{QuestionData[0].title}</Title>
+      <Title>{QuestionData[questionNo].title}</Title>
       <ButtonGroup>
-        <Button style={{ width: '40%', minHeight: '200px', fontSize: '15pt' }}>
-          {QuestionData[0].answerA}
+        <Button
+          onClick={() => handleClickButton(1, QuestionData[questionNo].type)}
+          style={{ width: '40%', minHeight: '200px', fontSize: '15pt' }}
+        >
+          {QuestionData[questionNo].answerA}
         </Button>
         <Button
+          onClick={() => handleClickButton(0, QuestionData[questionNo].type)}
           style={{
             width: '40%',
             minHeight: '200px',
@@ -25,7 +54,7 @@ function Question() {
             marginLeft: '20px',
           }}
         >
-          {QuestionData[0].answerB}
+          {QuestionData[questionNo].answerB}
         </Button>
       </ButtonGroup>
     </Wrapper>
