8f5bcab1c93996e20b96c3b7de59ec678c48035d
jiho9118 <mexocoxon@gmail.com>
2022-12-26 22:38:11 +0900

    Question.js 설정 및 질문지 생성

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Button, ProgressBar } from 'react-bootstrap';
+import { QuestionData } from '../assets/data/QuestionData';
 
 function Question() {
   return (
-    <div>Question</div>
-  )
+    <Wrapper>
+      <ProgressBar
+        striped
+        variant='danger'
+        now={80}
+        style={{ marginTop: '20px' }}
+      />
+      <Title>{QuestionData[0].title}</Title>
+      <ButtonGroup>
+        <Button style={{ width: '40%', minHeight: '200px', fontSize: '15pt' }}>
+          {QuestionData[0].answerA}
+        </Button>
+        <Button
+          style={{
+            width: '40%',
+            minHeight: '200px',
+            fontSize: '15pt',
+            marginLeft: '20px',
+          }}
+        >
+          {QuestionData[0].answerB}
+        </Button>
+      </ButtonGroup>
+    </Wrapper>
+  );
 }
 
-export default Question
+export default Question;
+
+const Wrapper = styled.div`
+  height: 100vh
+  width: 100%
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  text-align: center;
+  margin-top: 40px;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
+`;
