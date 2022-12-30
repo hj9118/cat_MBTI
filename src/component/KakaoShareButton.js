a1024047502600b540e835aaca46598481b7e20a
jiho9118 <mexocoxon@gmail.com>
2022-12-30 18:56:25 +0900

    컴포넌트 내 데이터 연결

--- src/component/KakaoShareButton.js
+++ src/component/KakaoShareButton.js
@@ -1,52 +1,48 @@
 import React, { useEffect } from 'react';
-import { Button } from 'react-bootstrap';
-const {Kakao} = window;
+import Button from 'react-bootstrap/Button';
+const { Kakao } = window;
 
-const KakaoShareButton = () => {
-  const url = 'https://oneuleun-cat-mbti.netlify.app'
-  const resultUrl = window.location.href
-
-  console.log('url test', resultUrl, url)
+const KakaoShareButton = ({ data }) => {
+  const url = 'https://oneuleun-cat-mbti.netlify.app';
+  const resultUrl = window.location.href;
 
   useEffect(() => {
     Kakao.cleanup();
-    Kakao.init("81522c980c69447be9daf07c0155957b")
+    Kakao.init('81522c980c69447be9daf07c0155957b');
     console.log(Kakao.isInitialized());
-  },[]);
+  }, []);
 
-const shareKakao = () => {
-  Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: '예비집사 판별기 결과',
-      description: '예비 집사님이 고양이를 키운다면',
-      imageUrl:
-        'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-      link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        webUrl: 'test',
-      },
-    },
-    
-    buttons: [
-      {
-        title: '테스트 하러 가기',
+  const shareKakao = () => {
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '예비집사 판별기 결과',
+        description: `예비 집사님이 고양이를 키운다면 가장 잘맞는 고양이는 ${data.name}입니다.`,
+        imageUrl: url + data.image,
         link: {
-          mobileWebUrl: url,
-          webUrl: url,
+          mobileWebUrl: resultUrl,
+          webUrl: resultUrl,
         },
       },
-    ],
-  });
-}
+      buttons: [
+        {
+          title: '나도 테스트 하러가기',
+          link: {
+            mobileWebUrl: url,
+            webUrl: url,
+          },
+        },
+      ],
+    });
+  };
+
   return (
-    <>
-      <Button
-        style={{ fontfamliy: 'IM_HyeMin', width: 170, marginLeft: '20px' }}
-      >
-        카카오톡 공유하기
-      </Button>
-    </>
+    <Button
+      onClick={shareKakao}
+      style={{ fontFamily: 'IM_HyeMin', width: 170, marginLeft: '20px' }}
+    >
+      카카오톡 공유하기
+    </Button>
   );
 };
 
